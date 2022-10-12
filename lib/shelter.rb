a99cfad738230b72517412fbae1fffd70f7b9c92
Elle Majors <hellolovely@ellemajors>
2022-10-12 10:09:29 -0600

    Added attr_reader

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,14 +1,23 @@
 class Shelter
+  attr_reader :name,
+              :capacity,
+              :pets
+
   def initialize (name, capacity)
     @name = name
     @capacity = capacity
+    @pets = []
   end
-
-    def name
-      @name
-    end
-
-    def capacity
-      @capacity
-    end
 end
+    # def name
+    #   @name
+    # end
+    #
+    # def capacity
+    #   @capacity
+    # end
+    #
+    # def pets
+    #   @pets
+    # end
+# end
